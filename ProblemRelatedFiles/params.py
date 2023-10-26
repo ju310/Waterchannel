517ada99662c1c79aaf05e014e8107384991b778
Judith Angel <judith.angel@tuhh.de>
2023-10-26 15:57:01 +0200

    correction observation at 'wrong' sensor positions; enable reading observation from measurement data

--- ProblemRelatedFiles/params.py
+++ ProblemRelatedFiles/params.py
@@ -24,6 +24,9 @@ class params:
         # Turn on/off test for gradient descent method. Start with exact b.
         self.test = True
 
+        # Use measurement data or simulated data.
+        self.measurementData = False
+
         # Put noise on observation.
         self.noise = 0
         # self.noise = 1
@@ -66,9 +69,6 @@ class params:
         # self.gamma = 1
         self.delta = 1 - self.gamma
 
-        # Use coarse propagator for serial gradient descent.
-        self.coarse_in_serial = False
-
         # Maximum number of iterations in gradient descent.
         if self.test:
             self.jmax = 2
@@ -77,6 +77,9 @@ class params:
 
         # Time step.
         self.dt = 1e-3
+
+        # Number of points in time.
+        N = int(self.T_N/self.dt) + 1
 
         # Number of grid points in space.
         self.M = 17*4
@@ -87,31 +90,35 @@ class params:
         self.xmin = 1.5
         # self.xmax = 20
         self.xmax = 15  # Matches better with measurements.
-        # self.pos = [3.5, 6, 8.5]  # Sensor positions
-        self.pos = [3.5, 6]
-        # self.pos = [2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
+        if self.measurementData:
+            self.pos = [3.5, 6, 8.5]  # Sensor positions
+        else:
+            self.pos = [3.5, 6]
+            # self.pos = [2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
         self.H = 0.3  # Water level at rest.
         self.lbc = leftbc(pathbc).f  # CubicSpline
 
         # Load observation, exact bathymetry and parameters from hdf5 file.
-        with h5py.File(path, "r") as f:
+        if not self.measurementData:
 
-            b_exact_fine = np.array(f["b_exact"])
-            # TODO: Change when using real data.
-            # TODO: Cut of first 3s of data.
-            H_sensor = np.array(f["H_sensor"])
-            # y_d_fine = np.array(f["H_sensor"])
-            self.h_array_full = np.array(f["h"])  # Computed h.
-            # y_d_fine = self.h_array_full.copy() + b_exact_fine
-            pos_p = np.array(f["pos"])
-            dt_p = f.attrs.get("dt")
-            M_p = f.attrs.get("M")
-            T_N_p = f.attrs.get("T_N")  # Final time.
-            t_array_fine = np.array(f["t_array"])
-            kappa_p = f.attrs.get("kappa")
-            g_p = f.attrs.get("g")
-            xmin_p = f.attrs.get("xmin")
-            xmax_p = f.attrs.get("xmax")
+            with h5py.File(path, "r") as f:
+
+                b_exact_fine = np.array(f["b_exact"])
+                # TODO: Change when using real data.
+                # TODO: Cut of first 3s of data.
+                H_sensor = np.array(f["H_sensor"])
+                # y_d_fine = np.array(f["H_sensor"])
+                self.h_array_full = np.array(f["h"])  # Computed h.
+                # y_d_fine = self.h_array_full.copy() + b_exact_fine
+                pos_p = np.array(f["pos"])
+                dt_p = f.attrs.get("dt")
+                M_p = f.attrs.get("M")
+                T_N_p = f.attrs.get("T_N")  # Final time.
+                t_array_fine = np.array(f["t_array"])
+                kappa_p = f.attrs.get("kappa")
+                g_p = f.attrs.get("g")
+                xmin_p = f.attrs.get("xmin")
+                xmax_p = f.attrs.get("xmax")
 
         # ---------------------------------------------------------------------
         # Check if parameters match the ones from the hdf5 file.
@@ -165,53 +172,63 @@ class params:
         b_exct_field.change_scales(1)
         self.b_exact = np.copy(b_exct_field['g'])
 
-        def eval_at_pos(sol, pos):
-
-            temp = []
-
-            for i in range(len(pos)):
-                sol_int = sol(x=pos[i])
-                temp.append(float(sol_int.evaluate()['g']))
-
-            return temp
-
-        # TODO: Change when using real data.
-        # prefix = 'ProblemRelatedFiles/WaterchannelData/'
-        # filename = 'Tiefe=0,3_A=40_F=0,35'
-        # lbc = leftbc(prefix+filename+".txt")
-        # dataObject = data(prefix+filename+".txt")
-        # H = 0.3
-        # H_sensor[0] = H+dataObject.f[0](self.t_array)
-
-        # Assuming same time step in observation and here.
-        self.y_d = np.zeros((self.t_array.size, self.M))
+        self.y_d = np.zeros((N, self.M))
         x = dist.local_grid(xbasis)
-        H_field = dist.Field(bases=xbasis)
 
-        if wrongpos:
+        if self.measurementData:
 
-            for n in range(N):
+            # Load observation data from text file.
+            dataObject = data(pathbc+".txt")
 
-                H_field.change_scales(1)
-                H_field["g"] = np.copy(self.h_array_full[n] + self.b_exact)
-                H_pos = eval_at_pos(H_field, self.pos)
+            for p in range(len(self.pos)):
+
+                H_sensor = self.H + dataObject.f[p](self.t_array)
+                i = np.argmin(abs(x-self.pos[p]))
+                self.y_d[:, i] = H_sensor
+        else:
+
+            # Put simulation data from hdf5 file on the coarser grid.
+
+            def eval_at_pos(sol, pos):
+
+                temp = []
+
+                for i in range(len(pos)):
+                    sol_int = sol(x=pos[i])
+                    temp.append(float(sol_int.evaluate()['g']))
+
+                return temp
+
+            H_field = dist.Field(bases=xbasis)
+
+            if wrongpos:
+
+                for n in range(N):
+
+                    H_field.change_scales(1)
+                    # Find corresponding index in fine time array.
+                    # This is the best way to do it as we need this loop
+                    # over time anyways.
+                    n_fine = np.argmin(abs(t_array_fine-self.t_array[n]))
+                    H_field["g"] = np.copy(
+                        self.h_array_full[n_fine] + b_exact_fine)
+                    H_pos = eval_at_pos(H_field, self.pos)
+
+                    for p in range(len(self.pos)):
+
+                        i = np.argmin(abs(x-self.pos[p]))
+                        self.y_d[n, i] = H_pos[p]
+
+            else:
+
+                n = np.argmin(abs(self.T_N-t_array_fine))
+                y_d_func = interpolate.CubicSpline(
+                    t_array_fine[:n+1], H_sensor[:n+1], axis=0)
 
                 for p in range(len(self.pos)):
 
                     i = np.argmin(abs(x-self.pos[p]))
-                    self.y_d[n, i] = H_pos[p]
-
-        else:
-
-            self.y_d = np.zeros((self.t_array.size, self.M))
-            n = np.argmin(abs(self.T_N-t_array_fine))
-            y_d_func = interpolate.CubicSpline(
-                t_array_fine[:n+1], H_sensor[:n+1], axis=0)
-
-            for p in range(len(self.pos)):
-
-                i = np.argmin(abs(x-self.pos[p]))
-                self.y_d[:, i] = y_d_func(self.t_array)[:, p]
+                    self.y_d[:, i] = y_d_func(self.t_array)[:, p]
 
         for p in range(len(self.pos)):
 
